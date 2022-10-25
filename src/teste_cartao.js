d884d37e964eae0325d7a05836f482e1d8db67a3
Bruno Volpe <bdva16@hotmail.com>
2022-10-25 12:21:45 -0300

    final aula 3

---
+++ src/teste_cartao.js
@@ -0,0 +1,17 @@
+//Regra VISA
+//Inicia com 4 seguido de 15 números
+4236584422365895
+
+//Regra Mastercard
+//inicia com 5, seguido de um digito entre 1 e 5, seguido de mais dois digitos
+//OU
+//inicia com 22, seguido de um digito entre 2 e 9, seguido de mais 1 digito
+//OU
+//inicia com 2, seguido de um digito entre 3 e 7, seguido de mais 2 digitos
+//seguido de mais 12 digitos
+5353535353535353
+5653535353535353
+
+2323232323232323
+2213232323232323
+2253232323232323
